920fd0cc4482606b7876bd6c1ceda921d901ee99
Jbugden97 <72581711+Jbugden97@users.noreply.github.com>
2022-06-13 14:35:40 +1000

    add refresh capability to button

--- dashboard_main.py
+++ dashboard_main.py
@@ -9,6 +9,7 @@ import Stock
 import numpy as np
 import pymongo
 from pymongo import MongoClient
+import pyautogui
 
 
 
@@ -91,6 +92,7 @@ with secndc1.container():
 
 
 
+
 with secndc2.container():
     st.subheader('Notes')
 
@@ -104,6 +106,7 @@ with secndc2.container():
             delete_button =st.button("Delete Note",key = idx)
             if delete_button:
                 collection_notes.delete_one({"Date": stock_note['Date'], "Note": stock_note['Note']})
+                pyautogui.hotkey("ctrl","F5")
             idx= idx+ 1
     
     note = st.text_area('Add a note..', '''
@@ -113,6 +116,7 @@ with secndc2.container():
     if add_button:
         post={"Ticker": ticker_select,"Note": note, "Date": current_date}
         collection_notes.insert_one(post)
+        pyautogui.hotkey("ctrl","F5")
 
 
     
